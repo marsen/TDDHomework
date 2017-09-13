e1a47fb2c6fdf47496eb82c66164790204cf520d
marklin <marklin@91app.com>
2017-09-13 23:52:29 +0800

    add test project

---
+++ HomeWork/ShoppingCart.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork
+{
+    public class ShoppingCart
+    {
+        public decimal CheckOut()
+        {
+            return 0M;
+        }
+    }
+}
---
+++ Homework.Tests/ShoppingCartTests.cs
@@ -0,0 +1,19 @@
+﻿using Xunit;
+using HomeWork;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork.Tests
+{
+    public class ShoppingCartTests
+    {
+        [Fact()]
+        public void CheckOutTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+    }
+}
