04a2388d8b23aab62b37542be3b9db9b69a604ea
marklin <marklin@91app.com>
2017-09-15 17:31:29 +0800

    run stylecop 綠燈

--- HomeWork/ShoppingCart.cs
+++ HomeWork/ShoppingCart.cs
@@ -6,8 +6,21 @@ using System.Threading.Tasks;
 
 namespace HomeWork
 {
+    /// <summary>
+    /// ShoppingCart
+    /// </summary>
     public class ShoppingCart
     {
+        /// <summary>
+        /// 目前最大集數
+        /// </summary>
+        private const int LastEpisode = 5;
+
+        /// <summary>
+        /// CheckOut
+        /// </summary>
+        /// <param name="bookList">bookList</param>
+        /// <returns>price</returns>
         public decimal CheckOut(List<Book> bookList)
         {
             var packageList = this.PackageBookList(bookList);
@@ -34,17 +47,25 @@ namespace HomeWork
                     default:
                         break;
                 }
+
                 totalPrice += (1 - discount) * packagePrice;                
             }
+
             return totalPrice;
         }
 
+        /// <summary>
+        /// 將不同的集數的書合成一套打包,
+        /// EX: 所有集數為 1,1,2,2,3,4,5
+        /// 會打包成 1,2,3,4,5 與 1,2 兩包       
+        /// </summary>
+        /// <param name="bookList">bookList</param>
+        /// <returns>打包後的書</returns>
         private List<List<Book>> PackageBookList(List<Book> bookList)
         {
-            var result = new List<List<Book>>();
-            const int lastEpisode = 5;
+            var result = new List<List<Book>>();            
             var sortedBookList = bookList.OrderBy(x => x.episode);
-            for (int i = 0; i < lastEpisode; i++)
+            for (int i = 0; i < LastEpisode; i++)
             {
                 var sameEpisodeBooks = bookList.Where(x => x.episode == i + 1).ToList();
                 var diff = sameEpisodeBooks.Count - result.Count;                
@@ -52,12 +73,12 @@ namespace HomeWork
                 {
                     result.Add(new List<Book>());
                 }
+
                 for (int k = 0; k < sameEpisodeBooks.Count; k++)
                 {
                     result.ElementAt(k).Add(sameEpisodeBooks.ElementAt(k));
                 }
             }
-            
 
             return result;
         }
