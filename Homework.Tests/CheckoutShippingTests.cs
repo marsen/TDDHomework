495bc4aaf7e6bc5b832b79668c1b557e04b54cb3
marklin <marklin@91app.com>
2017-10-09 21:20:55 +0800

    使用黑貓當物流 加入測試 紅燈

---
+++ Homework.Tests/CheckoutShippingTests.cs
@@ -0,0 +1,19 @@
+﻿using Xunit;
+using HomeWork;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork.Tests
+{
+    public class CheckoutShippingTests
+    {
+        [Fact()]
+        public void CheckOut_Shipping_With_BlackCat()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+    }
+}
