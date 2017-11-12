4469b031b40a35d4bde6d2aeb1faa92b484040bc
marklin <marklin@91app.com>
2017-11-13 02:10:04 +0800

    購物車已有書本 1 集 5 本 2 集 2 本 綠燈

--- Homework.Tests/ShoppingCartTests.cs
+++ Homework.Tests/ShoppingCartTests.cs
@@ -269,11 +269,17 @@ namespace HomeWork.Tests
         ////// Given: 購物車已有書本 1 集 6 本
         ////// When: 購物車結帳時
         ////// Then: 檢查不通過
+
+        ////// Scenario: 每一集最多買 5 本
+        ////// Given: 購物車已有書本 1 集 5 本 2 集 2 本
+        ////// When: 購物車結帳時
+        ////// Then: 檢查通過
         #endregion
 
         [Theory]
         [InlineData("1,2,3,4,5", true)]
         [InlineData("1,1,1,1,1,1", false)]
+        [InlineData("1,1,1,1,1,2,2", true)]
         public void Same_Episode_Up_to_5_Test(string bookEpisodeList, bool expected)
         {
             //// Arrange
